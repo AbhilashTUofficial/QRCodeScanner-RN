4dd7007c70f22539673a75254f569d6004ecf665
AbhilashTUofficial <abhilashstorm@gmail.com>
2023-01-03 07:51:55 +0530

    update

--- QR_Scanner_connector/App.js
+++ QR_Scanner_connector/App.js
@@ -1,7 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native'
 import React, { Component } from 'react'
 import NavigationController from './src/Controller/NavigationController'
-// https://github.com/moaazsidat/react-native-qrcode-scanner/issues/384#issuecomment-1285600911
 export class App extends Component {
   render() {
     return (
